9f3b36003aa31d97d419758d01f6b5f7d0bd8688
Nick Sullivan <nicholassull@gmail.com>
2022-06-03 16:50:35 -0700

    add padding to KegList

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -3,21 +3,27 @@ import PropTypes from "prop-types";
 import Keg from "./Keg";
 
 function KegList(props) {
+  const kegListStyling = {
+    padding: '0px 25px'
+  }
+  
   return ( 
     <React.Fragment>
       <hr />
-      {props.kegList.map((keg) =>
-        <Keg
-        whenKegClicked = {props.onKegSelection}
-        whenSellingPint = {props.onSellingPint}
-        name={keg.name}
-        brand={keg.brand}
-        price={keg.price}
-        flavor={keg.flavor}
-        pintsRemaining={keg.pintsRemaining}
-        id={keg.id}
-        key={keg.key} />
-      )}
+      <div style={kegListStyling}>
+        {props.kegList.map((keg) =>
+          <Keg
+          whenKegClicked = {props.onKegSelection}
+          whenSellingPint = {props.onSellingPint}
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          flavor={keg.flavor}
+          pintsRemaining={keg.pintsRemaining}
+          id={keg.id}
+          key={keg.key} />
+        )}
+      </div>
     </React.Fragment>
   );
 }
