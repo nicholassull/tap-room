d7f98522cd93f12424272517122ebddd020bda03
Nick Sullivan <nicholassull@gmail.com>
2022-06-03 12:44:00 -0700

    add KegControl.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Header from './Header';
+import KegControl from './KegControl';
 
 function App() {
   return (
     <React.Fragment>
-      
+      <Header />
+      <KegControl />
     </React.Fragment>
   );
 }
---
+++ src/components/KegControl.js
@@ -0,0 +1,15 @@
+class KegControl extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = { 
+
+  }
+  render() { 
+    return ( 
+
+    );
+  }
+}
+
+export default KegControl;
