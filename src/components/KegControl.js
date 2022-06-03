e0e5ff0cee30359e1db20678a12617b610028efe
Nick Sullivan <nicholassull@gmail.com>
2022-06-03 12:56:29 -0700

    add KegList.js and initial keg list

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,13 +1,32 @@
+import React from "react";
+import KegList from "./KegList";
+
 class KegControl extends Component {
   constructor(props) {
     super(props);
   }
   state = { 
-
+    mainKegList: [
+      {
+        name: "Night Sky",
+        brand: "Kile's Kombucha",
+        price: 5,
+        flavor: "London Fog"
+      },
+      {
+        name: "Shepherding Goats",
+        brand: "Flying High",
+        price: 6,
+        flavor: "Ambiguous"
+      }
+    ]
   }
-  render() { 
+  render() {
+    let currentlyVisibleState = <KegList kegList={this.state.mainKegList} onKegSelection={this.handleChangingSelectedKeg} />
     return ( 
-
+      <React.Fragment>
+        {currentlyVisibleState}
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/KegList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function KegList(props) {
+  return ( 
+    <React.Fragment>
+      <hr />
+      {props.}
+    </React.Fragment>
+  );
+}
+
+export default KegList;
