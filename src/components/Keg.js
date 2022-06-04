c4be69e97441dcafd01d8aa9a81b26620091de4f
Nick Sullivan <nicholassull@gmail.com>
2022-06-03 17:28:04 -0700

    add grid styling to Keg.js

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -3,21 +3,31 @@ import PropTypes from "prop-types";
 
 
 function Keg(props) {
+  // const containerStyling = {
+  //   display: 'grid',
+  //   alignItems: 'center'
+  // }
+  // const pintsContainerStyling = {
+  //   float: 'right'
+  // }
+  
   return ( 
     <React.Fragment>
-      <div>
-        <div onClick = {() => props.whenKegClicked(props.id)} className="keg-info">
-          <h3>{props.name} - ${props.price}</h3>
-          <p>Flavor: {props.flavor}</p>
-          <p>{props.brand}</p>
+      <div className="row">
+        <div className="keg-details col">
+          <div onClick = {() => props.whenKegClicked(props.id)} className="keg-info">
+            <h3>{props.name} - ${props.price}</h3>
+            <p>Flavor: {props.flavor}</p>
+            <p>{props.brand}</p>
+          </div>
         </div>
-        <div className="keg-pints-container">
+        <div className="keg-pints-container col">
           <h4>Pints</h4>
           <p>Remaining: {props.pintsRemaining}</p>
           <button className="btn btn-success" onClick={() => props.whenSellingPint(props.id)}>Sell Pint</button>
         </div>
-        <hr />
       </div>
+      <hr />
     </React.Fragment>
   );
 }
